b30b21e1800d9f40b747901d908e9cdd725debf0
Kent C. Dodds <me+github@kentcdodds.com>
2024-03-28 22:31:31 -0600

    vastly improve popstate handling and improve code overall

--- exercises/01.exercises/99.solution.final/src/index.js
+++ exercises/01.exercises/99.solution.final/src/index.js
@@ -4,6 +4,7 @@ import {
 	use,
 	useDeferredValue,
 	useEffect,
+	useReducer,
 	useRef,
 	useState,
 	useTransition,
@@ -21,19 +22,37 @@ function fetchContent(location) {
 
 const moduleBaseURL = '/js/src'
 
-let callServer
-
-const initialLocation = getGlobalLocation()
-const initialContentPromise = RSC.createFromFetch(
-	fetchContent(initialLocation),
-	{ moduleBaseURL, callServer: (id, args) => callServer?.(id, args) },
-)
-
 function generateKey() {
 	return Date.now().toString(36) + Math.random().toString(36).slice(2)
 }
 
+function updateContentKey() {
+	console.error('updateContentKey called before it was set!')
+}
 const contentCache = new Map()
+
+function createFromFetch(fetchPromise) {
+	return RSC.createFromFetch(fetchPromise, { moduleBaseURL, callServer })
+}
+
+async function callServer(id, args) {
+	// using the global location to avoid a stale closure over the location
+	const fetchPromise = fetch(getGlobalLocation(), {
+		method: 'POST',
+		headers: { Accept: 'text/x-component', 'rsc-action': id },
+		body: await RSC.encodeReply(args),
+	})
+	const actionResponsePromise = createFromFetch(fetchPromise)
+	const newContentKey = generateKey()
+	contentCache.set(newContentKey, actionResponsePromise)
+	updateContentKey(newContentKey)
+	const { returnValue } = await actionResponsePromise
+	return returnValue
+}
+
+const initialLocation = getGlobalLocation()
+const initialContentPromise = createFromFetch(fetchContent(initialLocation))
+
 let initialContentKey = window.history.state?.key
 if (!initialContentKey) {
 	initialContentKey = generateKey()
@@ -42,79 +61,79 @@ if (!initialContentKey) {
 contentCache.set(initialContentKey, initialContentPromise)
 
 export function Root() {
+	const [, forceRender] = useReducer(() => Symbol(), Symbol())
 	const latestNav = useRef(null)
 	const [nextLocation, setNextLocation] = useState(getGlobalLocation)
 	const [contentKey, setContentKey] = useState(initialContentKey)
 	const [isPending, startTransition] = useTransition()
+
+	// update the updateContentKey function to the latest every render
+	useEffect(() => {
+		updateContentKey = newContentKey => {
+			startTransition(() => setContentKey(newContentKey))
+		}
+	})
 
 	const location = useDeferredValue(nextLocation)
 	const contentPromise = contentCache.get(contentKey)
 
 	useEffect(() => {
 		function handlePopState() {
-			const key = window.history.state?.key
-			const contentPromise = key ? contentCache.get(key) : null
-			if (contentPromise) {
-				startTransition(() => setContentKey(key))
-			} else {
-				navigate(getGlobalLocation(), { updateHistory: false })
+			const nextLocation = getGlobalLocation()
+			setNextLocation(nextLocation)
+			const historyKey = window.history.state?.key ?? generateKey()
+
+			const thisNav = Symbol(`Nav for ${historyKey}`)
+			latestNav.current = thisNav
+
+			let nextContentPromise
+			const fetchPromise = fetchContent(nextLocation)
+			// create a promise chain that resolves when the stream is completely consumed
+			fetchPromise
+				// clone the response so createFromFetch can use it (otherwise we lock the reader)
+				// and wait for the text to be consumed so we know the stream is finished
+				.then(response => response.clone().text())
+				.then(() => {
+					contentCache.set(historyKey, nextContentPromise)
+					if (thisNav === latestNav.current) {
+						// trigger a rerender now that the updated content is in the cache
+						startTransition(() => forceRender())
+					}
+				})
+			nextContentPromise = createFromFetch(fetchPromise)
+
+			if (!contentCache.has(historyKey)) {
+				// if we don't have this key in the cache already, set it now
+				contentCache.set(historyKey, nextContentPromise)
 			}
+
+			updateContentKey(historyKey)
 		}
 		window.addEventListener('popstate', handlePopState)
 		return () => window.removeEventListener('popstate', handlePopState)
 	}, [])
 
-	callServer = async function callServer(id, args) {
-		// using the global location to avoid a stale closure over the location
-		const fetchPromise = fetch(getGlobalLocation(), {
-			method: 'POST',
-			headers: { Accept: 'text/x-component', 'rsc-action': id },
-			body: await RSC.encodeReply(args),
-		})
-		const actionResponsePromise = createFromFetch(fetchPromise)
-		const newContentKey = generateKey()
-		contentCache.set(newContentKey, actionResponsePromise)
-		startTransition(() => {
-			setContentKey(newContentKey)
-		})
-		const { returnValue } = await actionResponsePromise
-		return returnValue
-	}
-
-	function createFromFetch(fetchPromise) {
-		return RSC.createFromFetch(fetchPromise, { moduleBaseURL, callServer })
-	}
-
-	async function navigate(
-		nextLocation,
-		{ updateHistory = true, replace = false } = {},
-	) {
-		if (updateHistory) {
-			setNextLocation(nextLocation)
-		}
+	async function navigate(nextLocation, { replace = false, contentKey } = {}) {
+		setNextLocation(nextLocation)
 		const thisNav = Symbol()
 		latestNav.current = thisNav
 
-		const newContentKey = generateKey()
+		const newContentKey = contentKey ?? generateKey()
 		const nextContentPromise = createFromFetch(
 			fetchContent(nextLocation).then(response => {
 				if (thisNav !== latestNav.current) return
 				const newLocation = response.headers.get('x-location')
-				if (updateHistory) {
-					if (replace) {
-						window.history.replaceState({ key: newContentKey }, '', newLocation)
-					} else {
-						window.history.pushState({ key: newContentKey }, '', newLocation)
-					}
+				if (replace) {
+					window.history.replaceState({ key: newContentKey }, '', newLocation)
+				} else {
+					window.history.pushState({ key: newContentKey }, '', newLocation)
 				}
 				return response
 			}),
 		)
 
 		contentCache.set(newContentKey, nextContentPromise)
-		startTransition(() => {
-			setContentKey(newContentKey)
-		})
+		updateContentKey(newContentKey)
 	}
 
 	return h(
