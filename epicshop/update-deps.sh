96bf174e9f005e592aab266706479f3b0f33ae05
Kent C. Dodds <me+github@kentcdodds.com>
2024-04-29 10:29:01 -0600

    finish dep fix

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -4,5 +4,4 @@ cd ..
 rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
 npm install
 npm run setup
-npm run typecheck
-npm run lint --fix
+npm run lint -- --fix
