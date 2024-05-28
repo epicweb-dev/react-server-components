6eb659ac7b6e4ff3c5d24f2b004c2753ab077d74
Kent C. Dodds <me+github@kentcdodds.com>
2024-05-28 10:35:17 -0600

    remove typescript dep

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -1,5 +1,5 @@
-npx npm-check-updates --dep prod,dev --upgrade --root --reject react
-cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root --reject react
+npx npm-check-updates --dep prod,dev --upgrade --root --reject react,react-dom,eslint-plugin-react-hooks,react-server-dom-esm
+cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --root
 cd ..
 rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
 npm install
