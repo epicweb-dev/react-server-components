ff6c08ff546e8bdc4e0673458d1aebbcd3d01921
Kent C. Dodds <me+github@kentcdodds.com>
2024-09-16 17:17:41 -0600

    some more improvements to testing setup

--- exercises/01.init/01.problem.static/tests/solution.test.js
+++ exercises/01.init/01.problem.static/tests/solution.test.js
@@ -1,6 +1,39 @@
 import { test, expect } from '@playwright/test'
 
-test('should display the home page', async ({ page }) => {
+test('should display the home page and perform search', async ({ page }) => {
 	await page.goto('/')
 	await expect(page).toHaveTitle('Starship Deets')
+
+	// Check for the filter input
+	const filterInput = page.getByPlaceholder('filter ships')
+	await expect(filterInput).toBeVisible()
+
+	// Perform a search
+	await filterInput.fill('hopper')
+	await filterInput.press('Enter')
+
+	// Verify URL change with search params
+	await expect(page).toHaveURL('/?search=hopper')
+
+	// Verify filtered results
+	const shipLinks = page
+		.getByRole('list')
+		.first()
+		.getByRole('listitem')
+		.getByRole('link')
+	for (const link of await shipLinks.all()) {
+		await expect(link).toContainText('hopper', { ignoreCase: true })
+	}
+
+	// Find and click on a ship in the filtered list
+	const shipLink = shipLinks.first()
+	const shipName = await shipLink.textContent()
+	await shipLink.click()
+
+	// Verify URL change
+	await expect(page).toHaveURL(/\/[a-zA-Z0-9-]+/)
+
+	// Verify ship detail view
+	const shipTitle = page.getByRole('heading', { level: 2 })
+	await expect(shipTitle).toHaveText(shipName)
 })
--- exercises/05.actions/04.solution.revalidation/tests/playwright.config.js
+++ exercises/05.actions/04.solution.revalidation/tests/playwright.config.js
@@ -1,9 +1,19 @@
+import os from 'os'
+import path from 'path'
 import { defineConfig, devices } from '@playwright/test'
 
 const PORT = process.env.PORT || '3000'
 
+const tmpDir = path.join(os.tmpdir(), 'epicreact-server-components')
+
 export default defineConfig({
-	reporter: [['html', { open: 'never' }]],
+	outputDir: path.join(tmpDir, 'playwright-test-output'),
+	reporter: [
+		[
+			'html',
+			{ open: 'never', outputFolder: path.join(tmpDir, 'playwright-report') },
+		],
+	],
 	use: {
 		baseURL: `http://localhost:${PORT}/`,
 		trace: 'retain-on-failure',
